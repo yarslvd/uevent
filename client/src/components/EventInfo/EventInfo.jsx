23431f177fbef8d03713e591bc039dfe2b4469a5
yarslvd <yaroslavdoroshenko0@gmail.com>
2023-03-21 17:30:14 +0200

    created header of event page

--- client/src/components/EventInfo/EventInfo.jsx
+++ client/src/components/EventInfo/EventInfo.jsx
@@ -1,9 +1,67 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { Avatar, Button } from '@mui/material';
+
 import styles from './EventInfo.module.scss';
 
+// Implement follow/unfollow
+
 const EventInfo = () => {
+    const [isLikeActive, setIsLikeActive] = useState(false);
+
     return (
         <div className={styles.container}>
-            
+            <div className={styles.top}>
+                <div className={styles.time}>
+                    <div className={styles.date}>
+                        12 БЕР 2023
+                    </div>
+                    <div className={styles.clock}>
+                        <img src="/assets/clock_icon.png" alt="Time" />
+                        <span>18:00</span>
+                    </div>
+                </div>
+                <div className={styles.title}>
+                    <h1>Harry Styles</h1>
+                    <div onClick={() => console.log('Implement follow/unfollow')}><img src="/assets/follow_icon.png" alt="Follow event" /></div>
+                </div>
+                <div className={styles.details}>
+                    <div className={styles.item}>
+                        <img src="/assets/location_black_icon.png" alt="Location" />
+                        <a href='https://www.google.com/maps/place/Students+Palace+of+NTU+KhPI/@50.0067693,36.2481787,17.75z/data=!4m6!3m5!1s0x4127a0db465c2141:0xc08366351bcbc4a6!8m2!3d50.0067437!4d36.2490521!16s%2Fg%2F11r91b2l1' target='_blank' rel="noreferrer">Палац студентів НТУ “ХПІ”</a>
+                    </div>
+                    <div className={styles.item}>
+                        <img src="/assets/user_black_icon.png" alt="Company" />
+                        <Link>FIRST Concert Agency</Link>
+                    </div>
+                    <div className={styles.item}>
+                        <img src="/assets/price_black_icon.png" alt="Location" />
+                        <span>Від 450 грн.</span>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.bottom}>
+                <div className={styles.visitors}>
+                    <span>Підуть</span>
+                    <div className={styles.avatars}>
+                        {Array.from({ length: 19 }).map((_, index) => (
+                            <Link to={'/user/efe'}><Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" className={styles.avatar}/></Link>
+                        ))}
+                        <div onClick={() => console.log('Open modal with users who going to event')} className={styles.moreVisitors}>ще</div>
+                    </div>
+                </div>
+                <div className={styles.button_section}>
+                    <Button variant='contained' className={styles.buy_btn}>Купити</Button>
+                    <motion.div 
+                        className={styles.like_btn}
+                        whileTap={{ scale: 0.8 }}
+                        onClick={() => setIsLikeActive(!isLikeActive)}
+                    >
+                        <img src={isLikeActive ? "/assets/heart_filled_icon.png" : "/assets/heart_empty_icon.png"} alt="Like Event" />
+                    </motion.div>
+                </div>
+            </div>
         </div>
     )
 };
--- client/src/pages/EventPage/EventPage.jsx
+++ client/src/pages/EventPage/EventPage.jsx
@@ -1,5 +1,4 @@
-
-
+import { Spotify } from 'react-spotify-embed';
 import Layout from '../../components/Layout/Layout';
 import EventInfo from '../../components/EventInfo/EventInfo';
 
@@ -7,13 +6,82 @@ import styles from './EventPage.module.scss';
 
 const EventPage = () => {
     return (
-        <Layout>
-            <div className={styles.header}>
-                <div className={styles.image} style={{ backgroundImage: 'url(https://images.squarespace-cdn.com/content/v1/5c213a383c3a53bf091b1c36/3f825ca8-72ac-4c5d-b485-035b9ddb5364/h.jpeg)' }}></div>
-                <EventInfo />
+      <Layout>
+        <div className={styles.header}>
+          <div
+            className={styles.image}
+            style={{
+              backgroundImage:
+                "url(https://images.squarespace-cdn.com/content/v1/5c213a383c3a53bf091b1c36/3f825ca8-72ac-4c5d-b485-035b9ddb5364/h.jpeg)",
+            }}
+          ></div>
+          <div style={{ position: "sticky", top: 0 }}>
+            <EventInfo className={styles.info} />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.description}>
+              <h2>Про подію</h2>
+              <div className={styles.text}>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                sed diam voluptua. At vero eos et accusam et justo duo dolores
+                et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+                est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                invidunt ut labore et dolore magna aliquyam erat, sed diam
+                voluptua. At vero eos et accusam et justo duo dolores et ea
+                rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in
+                hendrerit in vulputate velit esse molestie consequat, vel illum
+                dolore eu feugiat nulla facilisis at vero eros et accumsan et
+                iusto odio dignissim qui blandit praesent luptatum zzril delenit
+                augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor
+                sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+                euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+                consequat. Duis autem vel eum iriure dolor in hendrerit in
+                vulputate velit esse molestie consequat, vel illum dolore eu
+                feugiat nulla facilisis at vero eros et accumsan et iusto odio
+                dignissim qui blandit praesent luptatum zzril delenit augue duis
+                dolore te feugait nulla facilisi. Nam liber tempor cum soluta
+                nobis eleifend option congue nihil imperdiet doming id quod
+                mazim placerat facer possim assum. Lorem ipsum dolor sit amet,
+                consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi
+                enim ad minim veniam, quis nostrud exerci tation ullamcorper
+                suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis
+                autem vel eum iriure dolor in hendrerit in vulputate velit esse
+                molestie consequat, vel illum dolore eu feugiat nulla facilisis.
+                At vero eos et accusam et justo duo dolores et ea rebum. Stet
+                clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+                dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+                sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+                eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+                gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+                amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                At accusam aliquyam diam diam dolore dolores duo eirmod eos
+                erat, et nonumy sed tempor et et invidunt justo labore Stet
+                clita ea et gubergren, kasd magna no rebum. sanctus sea sed
+                takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem
+                ipsum dolor sit amet, consetetur
+              </div>
             </div>
-        </Layout>
-    )
+            <div className={styles.embed}>
+                <Spotify link="https://open.spotify.com/track/6LmpYRchoYl3gRGbcQ7k8z?si=7ede7d8932e34c30" />
+                <iframe src="https://open.spotify.com/embed/track/6LmpYRchoYl3gRGbcQ7k8z?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
+          </div>
+        </div>
+        <div className={styles.description}></div>
+      </Layout>
+    );
 };
 
 export default EventPage;
