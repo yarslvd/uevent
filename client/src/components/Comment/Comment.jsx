c12c83ba64e996a50a4be3b64deedd138e7108a9
Maksym Hrynenko <mg06062003@gmail.com>
2023-04-11 00:49:11 +0300

    upd del comments

--- client/src/components/Comment/Comment.jsx
+++ client/src/components/Comment/Comment.jsx
@@ -1,24 +1,62 @@
-import { Avatar } from '@mui/material';
+import {Avatar, Button, Modal,} from '@mui/material';
+import ModeIcon from '@mui/icons-material/Mode';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import styles from './Comment.module.scss';
+import {useSelector} from "react-redux";
+import {useState} from "react";
 
-const Comment = ({ comment, user }) => {
+const Comment = ({ id, comment, user, setEditing, deleteComment }) => {
+    const { userInfo } = useSelector((state) => state.auth);
+    const [modalOpen, setModalOpen] = useState(false);
+    console.log(user)
+    const handleClose = () => {
+        setModalOpen(false);
+    }
 
     return (
-      <div className={styles.container}>
-        <div className={styles.top}>
-            <div className={styles.content}>
-                <Avatar alt={user.first_name} src="/static/images/avatar/1.jpg" />
-            </div>
-            <div className={styles.text}>
-                <span className={styles.username}>{`${user.first_name} ${user.last_name}`}</span>
-                <p className={styles.comment}>{comment}</p>
-            </div>
-        </div>
-        <div className={styles.bottom}>
-            19.03.03
-        </div>
-      </div>
+        <>
+            <Modal
+                open={modalOpen}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <>
+                    <div className={styles.modalContainer}>
+                        <div className={styles.modal}>
+                            <p >Ви впевнені , що хочете видалити коментар?</p>
+                            <div className={styles.modalButtons}>
+                                <Button variant='contained' onClick={() => {deleteComment(id);handleClose()}}>Так</Button>
+                                <Button variant='contained' onClick={() => handleClose()}>Ні</Button>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            </Modal>
+            <div className={styles.container}>
+                <div className={styles.top}>
+                    <div className={styles.content}>
+                        <Avatar alt={user.first_name} src={user.image?user.image:"/static/images/avatar/1.jpg"} />
+                    </div>
+                    <div className={styles.text}>
+                        <span className={styles.username}>{`${user.first_name} ${user.last_name}`}</span>
+                        <p className={styles.comment}>{comment}</p>
+                    </div>
+                    {(userInfo.id === user.id) && (
+                        <div className={styles.editButtons}>
+                            <ModeIcon
+                                className={styles.editButton}
+                                onClick={() => setEditing({id:id, content:comment})}/>
+                            <DeleteIcon className={styles.deleteButton} onClick={() => setModalOpen(true)}/>
+                        </div>
+                    )}
+                </div>
+                <div className={styles.bottom}>
+                    19.03.03
+                </div>
+              </div>
+        </>
     );
 };
 
