ae445840785a2d20e04dbc78f1e729e1542ec84e
Maksym Hrynenko <mg06062003@gmail.com>
2023-04-15 21:52:50 +0300

    handle invalid id for organization page

--- client/src/pages/OrganizationPage/OrganizationPage.jsx
+++ client/src/pages/OrganizationPage/OrganizationPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useParams } from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
 import { Button, useMediaQuery, Pagination } from '@mui/material';
 import { useSelector } from 'react-redux';
 
@@ -13,6 +13,7 @@ import { useGetEventsQuery } from '../../redux/api/fetchEventsApi';
 import styles from './OrganizationPage.module.scss';
 
 const OrganizationPage = () => {
+    const navigate = useNavigate()
     const { id } = useParams();
     const matches = useMediaQuery('(max-width:800px)');
 
@@ -40,11 +41,19 @@ const OrganizationPage = () => {
         });
     }
 
+    if (!isLoading && !data.organizer) {
+        navigate("/404")
+    }
+    console.log("Data:", data)
     return (
+        <>
+        {
+        !isLoading && data.organizer &&
         <Layout>
             <div className={styles.container}>
                 <div className={styles.header}>
-                    <div className={styles.image} style={{backgroundImage: `url(${!isLoading && !isError && data.organizer.poster})`}}></div>
+                    <div className={styles.image}
+                         style={{backgroundImage: `url(${!isLoading && !isError && data.organizer.poster})`}}></div>
                     <div className={styles.info}>
                         <div className={styles.content}>
                             <h3>{!isLoading && !isError && data.organizer.name}</h3>
@@ -53,7 +62,8 @@ const OrganizationPage = () => {
                             </div>
                             <div className={styles.email}>
                                 <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EmailIcon">
-                                    <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"></path>
+                                    <path
+                                        d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"></path>
                                 </svg>
                                 <a href={`mailto:${!isLoading && !isError && data.organizer.email}`}>{!isLoading && !isError && data.organizer.email}</a>
                             </div>
@@ -70,23 +80,26 @@ const OrganizationPage = () => {
                 <div className={styles.outerContainer}>
                     <div className={styles.title}>
                         <h2>Organization Events</h2>
-                        <img src="/assets/popularEvents_illustration.svg" alt="" />
+                        <img src="/assets/popularEvents_illustration.svg" alt=""/>
                     </div>
                     <div className={styles.events_container}>
                         <div className={styles.events}>
                             {!isLoadingEvents && !isErrorEvents && dataEvents.events.rows.map((el, index) => (
-                            matches ?
-                                <Card {...el} key={index}/> :
-                                <WideCard {...el} key={index}/> 
+                                matches ?
+                                    <Card {...el} key={index}/> :
+                                    <WideCard {...el} key={index}/>
                             ))}
                         </div>
-                        {!isLoadingEvents && !isErrorEvents && dataEvents.events.pages > 1 && 
-                            <Pagination count={dataEvents.events.pages} size={matches ? 'small' : 'large'} onChange={handlePageChange} className={styles.pagination}/>
+                        {!isLoadingEvents && !isErrorEvents && dataEvents.events.pages > 1 &&
+                            <Pagination count={dataEvents.events.pages} size={matches ? 'small' : 'large'}
+                                        onChange={handlePageChange} className={styles.pagination}/>
                         }
                     </div>
                 </div>
-            </div>                
+            </div>
         </Layout>
+        }
+        </>
     )
 }
 
